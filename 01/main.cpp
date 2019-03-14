cbb7a0c2448fdf077ab88cfda2609c42f4477f3f
Homyakin <tox.home@yandex.ru>
2019-03-14 23:47:25 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,4 +1,18 @@
-int main()
+#include <iostream>
+#include "numbers.dat"
+
+int main(int argc, char* argv[])
 {
+	if(argc == 0 || argc % 2 != 0)
+	{
+		std::cout << -1;
+		return 0;
+	}
+	
+	for(int i = 0; i < Size; ++i)
+	{
+		//std::cout << Data[i] << " ";
+	}
+	std::cout << Size;
 	return 0;
 }
