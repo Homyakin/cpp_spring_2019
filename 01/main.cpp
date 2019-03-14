9feaa95420480ec6f204908b894415bfe5502744
Homyakin <tox.home@yandex.ru>
2019-03-14 23:49:30 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "numbers.dat"
+#include <vector>
 
 int main(int argc, char* argv[])
 {
@@ -8,10 +9,18 @@ int main(int argc, char* argv[])
 		std::cout << -1;
 		return 0;
 	}
-	
+	std::vector<int> first;
+	std::vector<int> second;
+	for(int i = 0; i < argc; i+=2)
+	{
+		if(argv[i] <= argv[i+1]
+		{
+			
+		}
+	}
 	for(int i = 0; i < Size; ++i)
 	{
-		//std::cout << Data[i] << " ";
+		std::cout << Data[i] << " ";
 	}
 	std::cout << Size;
 	return 0;
