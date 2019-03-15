1d1f85814321a1a9cfd3a2f66aa850972ec57b2a
Homyakin <tox.home@yandex.ru>
2019-03-15 09:32:58 +0300

    Upd

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "numbers.dat"
 #include <vector>
+#include <cstdlib>
+
+
 
 int main(int argc, char* argv[])
 {
@@ -11,11 +14,48 @@ int main(int argc, char* argv[])
 	}
 	std::vector<int> first;
 	std::vector<int> second;
+	int intervals = 0;
 	for(int i = 0; i < argc; i+=2)
 	{
-		if(argv[i] <= argv[i+1]
+		int a = std::atoi(argv[i]), b = std::atoi(argv[i + 1]);
+		if(a <= b)
 		{
-			
+			if(intervals)
+			{
+				int j = 0;
+				while(j < intervals && first[j] < a) ++j;
+				int k = j;
+				while(k < intervals && second[k] < b) ++k;
+				bool f = false, s = false;
+				if(!(a >= first[j] && a <= second[j]))
+				{
+					++j;
+					f = true;
+				}
+				if(!(b >= first[k] && b <= second[k]))
+				{
+					++k;
+					s = true;
+				}
+				if(j < intervals && f)
+					first.insert(first.begin() + j,a);
+				else if(f)
+					first.push_back(a);
+				if(k < intervals && s)
+					second.insert(first.begin() + k, b);
+				else if(s)
+					second.push_back(b);
+				if(k - j && (s || f))
+				{
+					first.erase(first.begin() + j + 1, first.begin() + k - 1);
+				}
+				
+			}else
+			{
+				first.push_back(a);
+				second.push_back(b);
+				++intervals;
+			}
 		}
 	}
 	for(int i = 0; i < Size; ++i)
