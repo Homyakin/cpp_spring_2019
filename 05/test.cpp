7ef8c077aef89245f46e959800a1a1df35b518fe
Homyakin <tox.home@yandex.ru>
2019-04-19 10:54:21 +0300

    Improvement

--- 05/test.cpp
+++ 05/test.cpp
@@ -2,55 +2,39 @@
 #include <condition_variable>
 #include <mutex>
 #include <thread>
-
+#include <string>
 
 std::condition_variable dataReady;
 std::mutex m;
 bool last_ping = false;
-int pong_count = 0;
+bool data = false;
 const int N = 500000;
 
-void ping()
+void say(std::string word, bool ping)
 {
 	for (int i = 0; i < N; ++i)
 	{
-		if (!last_ping)
+		if (last_ping != ping)
 		{
 			std::lock_guard<std::mutex> lock(m);
-			std::cout << "ping\n";
-			last_ping = true;
+			std::cout << word << std::endl;
+			last_ping = ping;
 		}
 		else
 		{
 			--i;
 		}
 	}
-}
-
-void pong()
-{
-	for(pong_count = 0; pong_count < N; ++pong_count)
-	{
-		if (last_ping)
-		{
-			std::lock_guard<std::mutex> lock(m);
-			std::cout << "pong\n";
-			last_ping = false;
-		}
-		else
-		{
-			--pong_count;
-		}
-	}
-	dataReady.notify_all();
+	data = true;
+	dataReady.notify_one();
 }
 
 int main()
 {
 	std::unique_lock<std::mutex> lock(m);
-	std::thread t1(ping);
-	std::thread t2(pong);
-	while (pong_count != N)
+	std::thread t1(say, "ping", true);
+	std::thread t2(say, "pong", false);
+	while (!data)
 		dataReady.wait(lock);
 	t1.join();
 	t2.join();
