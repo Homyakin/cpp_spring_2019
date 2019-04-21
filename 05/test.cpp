be68174dfa8405382b45de4fd399f0a6832d696f
Homyakin <tox.home@yandex.ru>
2019-04-21 22:44:38 +0300

    Update test.cpp

--- 05/test.cpp
+++ 05/test.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <condition_variable>
 #include <mutex>
 #include <thread>
 #include <string>
 
-std::condition_variable dataReady;
 std::mutex m;
 bool last_ping = false;
-bool data = false;
 const int N = 500000;
 
 void say(std::string word, bool ping)
@@ -25,20 +22,12 @@ void say(std::string word, bool ping)
 			--i;
 		}
 	}
-	if (!ping)
-	{
-		data = true;
-		dataReady.notify_one();
-	}
 }
 
 int main()
 {
-	std::unique_lock<std::mutex> lock(m);
 	std::thread t1(say, "ping", true);
 	std::thread t2(say, "pong", false);
-	while (!data)
-		dataReady.wait(lock);
 	t1.join();
 	t2.join();
 	return 0;
